965e3d4c4ae85c1b07eb52dfe5bd4b78a40c0194
Evgenii Zhemchugov <evgenii.zhemchugov@cern.ch>
2019-09-13 17:50:37 +0200

    Create Monte Carlo production chain
    Configuration/Generator/python/WToTauNu_13TeV_pythia8_cff: this is
    WToLNu_13TeV_pythia8_cff with W decays to other leptons removed.
    
    Configuration/Generator/python/__init__.py is modified to make the CMSSW
    to look for modules in $CMSSW_RELEASE_BASE. Thus there is no need to
    keep the unmodified parts of the generator configuration in this
    repository.
    
    work/mc/driver.sh: generates the files for cmsRun and makes a test run
    on few events.
    
    work/mc/fwjr-merge: a script to merge FrameworkJobReport.xml from
    several runs of cmsRun.
    
    work/mc/WToTauNu_13TeV_crab.py: CRAB input file.
    
    work/mc/WToTauNu_13TeV.sh: a script to be run by CRAB.
    
    work/mc/TreeMaker.py: an example of TreeMaker input for MC processing.

---
+++ src/Configuration/Generator/python/__init__.py
@@ -0,0 +1,6 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Configuration/Generator/',1)[0])+'/cfipython/slc7_amd64_gcc700/Configuration/Generator')
+
+
+__path__.append(os.getenv('CMSSW_RELEASE_BASE') + '/python/Configuration/Generator')
---
+++ work/mc/TreeMaker.py
@@ -0,0 +1,63 @@
+import re
+
+import XRootD.client
+
+import FWCore.ParameterSet.Config as cms
+# Define the CMSSW process
+process = cms.Process("TreeMaker")
+
+# Load the standard set of configuration modules
+
+process.load('FWCore.MessageLogger.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+
+#process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
+#from PhysicsTools.PatAlgos.tools.tauTools import * # to get the most up-to-date discriminators when using patTaus
+#switchToPFTauHPS(process)
+
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag.globaltag=autoCond['run2_mc']
+
+
+
+## Message Logger settings
+#process.load("FWCore.MessageService.MessageLogger_cfi")
+
+# How many events to process
+
+process.maxEvents = cms.untracked.PSet(
+   input = cms.untracked.int32(-1)
+)
+# Define the input source
+
+source = '/data/phedex/user/ezhemchu/crab/WToTauNu_13TeV/WToTauNu_13TeV/crab_WToTauNu_13TeV/190830_120718/0000'
+status, dirlist = XRootD.client.FileSystem('cms-phedex.lxfarm.mephi.ru').dirlist(source)
+files = [ source + '/' + f.name for f in dirlist if re.search('\.root$', f.name) ]
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(*files),
+                            skipEvents = cms.untracked.uint32(0)
+)
+
+process.treemaker = cms.EDAnalyzer("TreeMaker")
+
+process.load('TauPolarization.TreeMaker.treeMaker_MC-April_cfi')
+
+process.p = cms.Path(process.treemaker)
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("test_MC_aod.root")
+                                   )
+
+#process.MessageLogger = cms.Service(
+#    'MessageLogger',
+#    destinations = cms.untracked.vstring('debug'),
+#    debugModules = cms.untracked.vstring('treemaker'),
+#    debug = cms.untracked.PSet(
+#      threshold = cms.untracked.string('DEBUG'),
+#    )
+#)
