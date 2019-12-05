4ea2bf073ee4b289c1c92483c68f4e11f4ef4b3c
Evgenii Zhemchugov <evgenii.zhemchugov@cern.ch>
2019-12-05 10:48:44 +0100

    work/mc: remove old configuration files
    Cleanup WRToTauNu, WToTauNu, ZToTauTau. Use driver.sh and crab instead.

--- work/mc/WRToTauNu/WRToTauNu_13TeV.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cmsRun -j WRToTauNu_13TeV_GEN-SIM.xml PSet.py
-cmsRun -j WRToTauNu_13TeV_GEN-SIM-RAW.xml WRToTauNu_13TeV_GEN-SIM-RAW.py
-cmsRun -j WRToTauNu_13TeV_GEN-SIM-RAW-RECO.xml WRToTauNu_13TeV_GEN-SIM-RAW-RECO.py
-
-./fwjr-merge WRToTauNu_13TeV_GEN-SIM{,-RAW{,-RECO}}.xml > FrameworkJobReport.xml
--- work/mc/WToTauNu/WToTauNu_13TeV.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cmsRun -j WToTauNu_13TeV_GEN-SIM.xml PSet.py
-cmsRun -j WToTauNu_13TeV_GEN-SIM-RAW.xml WToTauNu_13TeV_GEN-SIM-RAW.py
-cmsRun -j WToTauNu_13TeV_GEN-SIM-RAW-RECO.xml WToTauNu_13TeV_GEN-SIM-RAW-RECO.py
-
-./fwjr-merge WToTauNu_13TeV_GEN-SIM{,-RAW{,-RECO}}.xml > FrameworkJobReport.xml
